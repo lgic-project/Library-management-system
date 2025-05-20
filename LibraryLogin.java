0903c2a5ca54cc6cb923ec9494ad043c80131b45
askagodar <160038934+askagodar@users.noreply.github.com>
2025-05-20 23:22:37 +0545

    #

--- LibraryLogin.java
+++ LibraryLogin.java
@@ -1,31 +1,27 @@
-
+import java.util.Scanner;
 
 public class LibraryLogin {
     public static void main(String[] args) {
-        scanner input = new scanner(System.in);
-        string username = "admin";
-        string password = "1234";
+        Scanner input = new Scanner(System.in);
+        String username = "admin";
+        String password = "1234";
 
-        System.out.println("Library Management system - Login");
-        system.out.print("Enter username: ");
-        string user = input.nextLine();
+        System.out.println("Library Management System");
+        
+        System.out.print("Enter username: ");
+        String user = input.nextLine();
 
-        system.out.print("Enter password: ");
-        string pass = input.nextLine();
+        System.out.print("Enter password: ");
+        String pass = input.nextLine();
 
 
         if(user.equals(username) && pass.equals(password)) {
-            system.out.println("Login successful! Welcome to the Library Management system.");
+            System.out.println("Login successful! Welcome to the Library Management system.");
 
         }else{
-            system.out.println("Login failed! Incorrect username or password.");
+            System.out.println("Login failed! Incorrect username or password.");
         }
         input.close();
     }
 
-    private static class scanner {
-
-        public scanner() {
-        }
-    }
 }
